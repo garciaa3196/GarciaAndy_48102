0b32104c9985c3537ab327c1b92b90d31f8c4893
garciaa3196 <garciaa0461@gmail.com>
2016-10-27 03:10:27 -0700

    Signed-off-by: garciaa3196 <garciaa0461@gmail.com>

--- Project/Project 1/Checkers/main.cpp
+++ Project/Project 1/Checkers/main.cpp
@@ -1,7 +1,7 @@
 /* 
  * File:   main.cpp
  * Author: Andy Garcia
- * Created on October 24, 2016, 12:37 AM
+ * Created on October 23, 2016, 12:37 AM
  * Purpose: Playing Checkers
  */
 
@@ -18,46 +18,31 @@ using namespace std;    //Namespace used in System Library
 //Function Prototypes
 void input();
 void move();
-void posLeap();     //Possible Leap
-void gameover();
-void king();
-bool game;
-bool movcheck();
+void Leap();        //Handles Leaps
+void gameover();    //Ends Game
+void king();        //Determines whether a piece is a King
+bool game=true;          
+bool movcheck();    //Checks for valid move
 bool leap;
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    int rows,cols;  
+    int rows,cols,row1,col1,row2,col2;  
     ifstream in;    //Board File
-    ofstream out;   //Player File
     string line;
-    char Turn='B';
-    
+    char Turn='B';      //Player turn
+    int x,y;
+    char board[8][8];
     
     while(game){
         //Display Board
-        in.open("Board.dat");
-        in>>rows>>cols;
-        cout<<"Rows = "<<rows<<" Columns = "<<cols<<endl;
-
-        //Display Output / what is in the file to the output screen
-        //In addition add in row numbers and column numbers for the user
-        getline(in,line);
-        for(int row=1;row<=rows;row++){
-            getline(in,line);
-            cout<<row<<" "<<line<<endl;
-        }
-        cout<<"  ";
-        for(int col=1;col<=cols;col++){
-            cout<<col;
-        }
-        cout<<endl;
+        
         
         //Designate Turn
         if(Turn=='B'){              
             cout<<"Player 1's Turn"<<endl;    
-        }else if(Turn=='R'){
+        }else if(Turn=='W'){
             cout<<"Player 2's Turn"<<endl;
         }
         
@@ -66,79 +51,186 @@ int main(int argc, char** argv) {
         move();
         king();
         gameover();
-        
-        //Open player file and write inputs to file
-        out.open("Player.dat");
-        out>>rows>>cols;
-        cout<<"Rows = "<<rows<<" Columns = "<<cols<<endl;
-        
     }
     if(Turn=='B'){
-        cout<<"(Red) Player 2 Wins!"<<endl;
-    }else if(Turn=='R'){
+        cout<<"(White) Player 2 Wins!"<<endl;
+    }else if(Turn=='W'){
         cout<<"(Black) Player 1 Wins!"<<endl;
     }
     cout<<"Game Over"<<endl;
     
-    //Close the File
-    in.close();    
+    //Close file
+    in.close();
     
     //Exit 
     return 0;
 }
-
-void input(){             //Player takes their turn
+ 
+void input(int &row1,int &col1,int &row2,int &col2){  //Player takes their turn
     //Choosing a piece to move
-    int rowp,colp;
     cout<<"Select a piece \n";
     cout<<"Row = ";
-    cin>>rowp;
+    cin>>row1;
     cout<<"Column = ";
-    cin>>colp;
+    cin>>col1;
     
     //Input Validation
-    if(rowp<1||rowp>8||colp<1||colp>8){
+    if(row1<1||row1>8||col1<1||col1>8){
         cout<<"Invalid Value - Enter a value between 1 and 8"<<endl;
         cout<<"Select a piece \n";
         cout<<"Row = ";
-        cin>>rowp;
+        cin>>row1;
         cout<<"Column = ";
-        cin>>colp;
+        cin>>col1;
     }
     //Move Piece to another location
-    int rowm,colm;
     cout<<"Select a place to move your piece"<<endl;
     cout<<"Row = ";
-    cin>>rowm;
+    cin>>row2;
     cout<<"Column = ";
-    cin>>colm;
+    cin>>col2;
     //Input Validation
-    while(rowm<1||rowm>8||colp<1||colp>8){
+    while(row2<1||row2>8||col2<1||col2>8){
         cout<<"Invalid Value - Enter a value between 1 and 8"<<endl;
         cout<<"Select a place to move your piece \n";
         cout<<"Row = ";
-        cin>>rowm;
+        cin>>row2;
         cout<<"Column = ";
-        cin>>colm;
+        cin>>col2;
     }
     while(movcheck()==false){
         cout<<"Illegal Move!! "<<endl;
         input();
     }
 }
-bool movcheck(){
+void move(int &row1,int &col1,int &row2,int &col2,char &Turn,char (&board)[8][8]){
+    bool kPiece=false;
+    if (board[row1][col1]== 'B'||board[row1][row1]== 'W'){
+        bool kPiece=true;
+    }
+    board[8][8]=' ';
+    if(Turn=='B'){
+        if(kPiece==false){
+            board[row2][col2]='b';
+        }else if(kPiece==true){
+            board[row2][col2]='B';
+        }
+        Turn='W';
+    }
+    else if(Turn=='W'){
+        if(kPiece==false){
+            board[row2][col2]='w';
+        }else if(kPiece==true){
+            board[row2][col2]='W';
+        }
+        Turn='B';
+    }
+    if (leap==true){
+        Leap();
+    }
+}
+bool movcheck(int &row1,int &col1,int &row2,int &col2,char &Turn,char (&board)[8][8]){
+    //Backwards movement-Non-King
+    if(board[row1][col1]!='B'&&board[row1][col1]!='W'){
+        if(Turn=='B'||Turn=='W'){
+            if(row2<row1){
+                leap=false;
+                return false;
+            }
+        }
+    }
+    //Diagonal Movement
+    if(row1==row2||col1==col2){
+        leap=false;
+        return false;
+    }
+    //Only move 1 row and 1 column
+    if(((row2==row1 +1 || row2==row1 - 1)&&(col2>col1 + 1 || col2<col1 - 1))){
+        leap=false;
+        return false;
+    }
+    //Existing pieces in selected area
+    if(board[row2][col2]!=' '){
+        leap=false;
+        return false;
+    }
+    //Selected space contains no piece
+    if(board[row1][col1]==' '){
+        leap=false;
+        return false;
+    }
+    //Piece is leaping/jumping?
+    if(row2>row1 +1 ||row2<row1 -1){
+        //Too far of a Leap
+        if(row2>row1 +2 || row2<row1 -2){
+            leap=false;
+            return false;
+        }
+        //WORK IN PROGRESS
+    }
+    
     
 }
-void move(){
+void Leap(int &row1,int &col1,int &row2,int &col2,char &Turn,char (&board)[8][8]){
+    char ans;       //Choose to jump more than once
+    //Leap removes piece
+    if(row2>row1&&col2>col1){
+        board[row2-1][col2-1]=' ';
+    }
+    else if(row2>row1&&col2<col1){
+        board[row2-1][col2+1]=' ';
+    }
+    else if(row2<row1&&col2>col1){
+        board[row2+1][col2-1]=' ';
+    }
+    else if(row2<row1&&col2<col1){
+        board[row2+1][col2+1]=' ';
+    }
+    //Display Board and input changes
+    
+    //For more than 1 leap
+    do{
+        cout<<"You jumped once. Would you like to leap again, if possible?(Y/N)"<<endl;
+        cin>>ans;
+    }while(ans!='Y'&&ans!='y'&&ans!='N'&&ans!='n');
+    if(ans=='Y'||ans=='y'){
+        row1=row2;
+        col1=col2;
+        cout<<"Select the row and column where you'd like piece ("
+                <<row1<<","<<col1<<") to jump to"<<endl;
+        cin>>row2>>col2;
+    }
+    //Input Validation
+    while(row2<1||row2>8||col2<1||col2>8){
+        cout<<"Invalid Value - Enter a value between 1 and 8"<<endl;
+        cout<<"Select a place to move your piece \n";
+        cout<<"Row = ";
+        cin>>row2;
+        cout<<"Column = ";
+        cin>>col2;
+    }
+    if(ans=='N'||ans=='n'){
+        if(Turn=='B'){
+            Turn='W';
+        }
+        if(Turn=='W'){
+            Turn='B';
+        }
+    }
+    //WORK IN PROGRESS
     
 }
-void posLeap(){
-    
+void king(char (&board)[8][8]){
+    int King;
+    for(int King=1;King<8;King++){
+        if(board[1][King]=='b'){
+            board[1][King]='B';
+        }
+        if(board[8][King]=='w'){
+            board[8][King]='W';
+        }
+    }
 }
-void king(){
-    
-}
-bool game=true;
 void gameover(){
 
 }
--- Project/Project 1/Checkers/nbproject/Makefile-variables.mk
+++ Project/Project 1/Checkers/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_2-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_2-Windows
 CND_ARTIFACT_NAME_Debug=checkers
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/checkers
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_2-Windows/checkers
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_2-Windows/package
 CND_PACKAGE_NAME_Debug=checkers.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/checkers.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_2-Windows/package/checkers.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_2-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_2-Windows
 CND_ARTIFACT_NAME_Release=checkers
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/checkers
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_2-Windows/checkers
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_2-Windows/package
 CND_PACKAGE_NAME_Release=checkers.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/checkers.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_2-Windows/package/checkers.tar
 #
 # include compiler specific variables
 #
