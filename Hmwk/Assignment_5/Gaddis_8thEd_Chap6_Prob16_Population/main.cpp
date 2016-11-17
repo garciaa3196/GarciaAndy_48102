e44ec6382900d8505b2e1850b1aae5cc9709edd3
garciaa3196 <garciaa0461@gmail.com>
2016-11-17 10:48:34 -0800

    Signed-off-by: garciaa3196 <garciaa0461@gmail.com>

--- Hmwk/Assignment_5/Gaddis_8thEd_Chap6_Prob16_Population/main.cpp
+++ Hmwk/Assignment_5/Gaddis_8thEd_Chap6_Prob16_Population/main.cpp
@@ -7,7 +7,6 @@
 
 //System Libraries Here
 #include <iostream>     //Input/Output Objects
-#include <iomanip>      //Formatting
 using namespace std;
 
 //User Libraries Here
@@ -20,11 +19,44 @@ int popSize(int,float,float);
 //Program Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    int iniPop,birth,death,years;
+    int Pop,birth,death,years;
     float bRate,dRate;
-
+    
+    //Input Values
+    cout<<"This program measures population as a function of time"<<endl;
+    cout<<"Population Size?"<<endl;
+    cin>>Pop;
+    cout<<"Birth Rate per year?"<<endl;
+    cin>>birth;
+    cout<<"Death Rate per year?"<<endl;
+    cin>>death;
+    cout<<"How many years pass?"<<endl;
+    cin>>years;
+    
+    //Input Validation
+    if(Pop<2||birth<0||death<0||years<1){
+        cout<<"Invalid value - Please enter a valid value"<<endl;
+        cout<<"Population Size?"<<endl;
+        cin>>Pop;
+        cout<<"Birth Rate per year?"<<endl;
+        cin>>birth;
+        cout<<"Death Rate per year?"<<endl;
+        cin>>death;
+        cout<<"How many years pass?"<<endl;
+        cin>>years;
+    }
+    
     //Process Values -> Map Inputs to Outputs
+    bRate=1.0f*birth/Pop;
+    dRate=1.0f*death/Pop;
+    for(int year=1;year<=years;year++){
+        Pop=popSize(Pop,dRate,bRate);
+        cout<<"Year "<<year<<" = population of "<<Pop<<endl;
+    }
     
     //Exit
     return 0;
+}
+int popSize(int Pop,float death,float birth){
+    return Pop*(1+birth-death);
 }
