8531d8c7d8e9c79734380b50423b8b8502d6bb5e
garciaa3196 <garciaa0461@gmail.com>
2016-12-08 12:39:21 -0800

    Signed-off-by: garciaa3196 <garciaa0461@gmail.com>

--- Project/Project 2/Economy Game/main.cpp
+++ Project/Project 2/Economy Game/main.cpp
@@ -17,9 +17,13 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void fillAry(string [],int);
+void fillAry(string [],int&,int);
+void fillDec(int[],int);
 void rules();
 void shuffle(int [],int);
+void prntAry(string [],int);
+void showRole(string[],int);
+
 
 //Program Execution Begins Here
 int main(int argc, char** argv) {
@@ -28,12 +32,9 @@ int main(int argc, char** argv) {
     
     //Declare Variables
     const int SIZE=52;
-    string deck[SIZE]={"1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","11C",
-                       "12C","13C","1S","2S","3S","4S","5S","6S","7S","8S","9S",
-                       "10S","11S","12S","13S","1D","2D","3D","4D","5D","6D",
-                       "7D","8D","9D","10D","11D","12D","13D","1H","2H","3H",
-                       "4H","5H","6H","7H","8H","9H","10H","11H","12H","13H"};
-    int play[utilize],menu,utilize;
+    int deck[SIZE];
+    string play[SIZE];
+    int menu,utilize;
     
     
     //Start Menu
@@ -45,7 +46,9 @@ int main(int argc, char** argv) {
     
     //Selection
     if(menu==1){
-        fillAry(play,utilize);
+        fillAry(play,utilize,SIZE);
+        fillDec(deck,SIZE);
+        showRole(play,utilize);
         
     }
     else if(menu==2){
@@ -54,39 +57,65 @@ int main(int argc, char** argv) {
     else{
         return 0;
     }
-    
-    //Input or initialize values
-    
-    //Process/Calculations
-    
-    //Display Output
-    
+        
 
     //Exit
     return 0;
 }
-void fillAry(string a[],int &utilize){
-    cout<<"How many players are playing? (up to 8)"<<endl;
+void fillAry(string a[],int &utilize,int size){
+    
+    cout<<"How many players are playing? (up to 8, ORDER MATTERS)"<<endl;
     cin>>utilize;
+    
     cout<<"What is the name of each player?"<<endl;
     for(int i=0;i<utilize;i++){
-        cin<<a[i];
+        cin>>a[i];
+    }
+}
+void fillDec(int a[],int size){
+    for(int i=0;i<size;i++){
+        a[i]=i+1;
     }
 }
 void rules(){
     cout<<"This card game is played with each player having a role. Player 1 is"
             " the King and Player 8 is the peasant. All other players are "
             "citizens. The deck is shuffled and all cards are dealt out "
-            " to the players. \n"
-            "To start, the King plays a card start a stack."
-            " This stack can be added to by other players. \n"
-            "Cards can only be added to the stack if they are higher in number "
-            "to the last card in the stack. \n"
-            "If the cards can't be dealt out equally to each player, "
+            "to the players. \n"
+            "- To start, the King plays a card start a stack. "
+            "This stack can be added to by other players. \n"
+            "- If the cards can't be dealt out equally to each player, "
             "the peasant will be given remaining card. \n"
-            "Players MAY play a double,triple,or quadruple if they have multiple"
-            " of the same card. \n"
-            "Before beginning, the peasant must give the king their highest card "
+            "- Cards can only be added to the stack if they are higher in number "
+            "to the last card in the stack. \n"
+            "- Go around to each player and ask if they wish to add to the stack. \n"
+            "- If nobody wants to add to the stack, the stack is wiped out and the last "
+            "player to add to the old stack begins a new stack. \n"
+            "- Aces wipe out a stack immediately and said player begins a new stack. \n"
+            "- Players MAY start a stack with a double,triple,or quadruple "
+            "if they have multiple of the same card. \n"
+            "- If the stack starts with a double,triple, or quadruple, you can"
+            " ONLY add to the stack if you play doubles,triples, or quadruples. "
+            "This includes Aces. \n"
+            "- Before beginning, the peasant must give the king their highest card "
             "while the king gives the peasant their lowest card. \n"
             "The goal of the game is to be the first to have no more cards. \n"
+            "The game ends when everyone but one player has no more cards. \n"
+            "The order in which run out of cards determines who the "
+            "king and peasant are next time."<<endl;
+}
+void prntAry(string a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<endl;
+    }
+}
+void showRole(string a[],int n){
+    for(int i=0;i<n;i++){
+        if(a[i]==a[0]){
+            cout<<"King: "<<a[i]<<endl;
+        }
+        else if(a[i]==a[7]){
+            cout<<"Peasant: "<<a[i]<<endl;
+        }
+    }
 }
